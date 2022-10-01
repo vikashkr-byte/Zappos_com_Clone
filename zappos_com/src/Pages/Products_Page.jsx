1c08a920ad4fb52728340a1d626880dec0ce71be
vikashkr-byte <vikashkr4git@gmail.com>
2022-10-01 12:26:12 +0530

    ho

--- zappos_com/src/Pages/Products_Page.jsx
+++ zappos_com/src/Pages/Products_Page.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ProductsData from '../Components/ProductsData'
 
 const Products_Page = () => {
-    console.log('ProductsData:', ProductsData)
+    console.log('ProductsData:', ProductsData[0].category.name)
+    console.log('ProductsData:', ProductsData[0].category.name)
   return (
     <div>Products_Page</div>
   )
