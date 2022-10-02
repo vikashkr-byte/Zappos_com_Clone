20bd286defe484f7a02a7dedfb20d16e3d728a56
vikashkr-byte <vikashkr4git@gmail.com>
2022-10-02 19:38:57 +0530

    final

--- zappos_com/src/Pages/ProductPageOG.jsx
+++ zappos_com/src/Pages/ProductPageOG.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   ButtonGroup,
+  Divider,
   Flex,
   Grid,
   GridItem,
@@ -13,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Footer_HomePage from "../Components/Footer_HomePage";
 import HomePage_Text_Carousel from "../Components/HomePage_Text_Carousel";
 import LoadingComponent from "../Components/LoadingComponent";
@@ -30,10 +32,10 @@ const ProductPageOG = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [total, setTotal] = useState(5);
   const [page, setPage] = useState(1);
-  console.log("products_data:", products_data);
+  // console.log("products_data:", products_data);
 
-let limit=8
-  console.log('isLoading:', isLoading)
+  let limit = 8;
+  // console.log("isLoading:", isLoading);
   useEffect(() => {
     setIsLoading(true);
 
@@ -45,25 +47,33 @@ let limit=8
       handleClickMen();
     }
     //  handleClickMen()
-  }, [categogo,page]);
+  }, [categogo, page]);
 
   const handleClickWomen = () => {
     setProducts_data([]);
-   
-    axios.get(`http://localhost:8080/products_data_women?_page=${page}&_limit=${limit}`).then((res) => {
-      setProducts_data(res.data);
-      console.log("res:", res.data);
-      setIsLoading(false);
-    });
+
+    axios
+      .get(
+        `http://localhost:8080/products_data_women?_page=${page}&_limit=${limit}`
+      )
+      .then((res) => {
+        setProducts_data(res.data);
+        console.log("res:", res.data);
+        setIsLoading(false);
+      });
     setCategogo("Women");
   };
   const handleClickMen = () => {
     setProducts_data([]);
-    axios.get(`http://localhost:8080/products_data_men?_page=${page}&_limit=${limit}`).then((res) => {
-      setProducts_data(res.data);
-      console.log("res:", res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get(
+        `http://localhost:8080/products_data_men?_page=${page}&_limit=${limit}`
+      )
+      .then((res) => {
+        setProducts_data(res.data);
+        console.log("res:", res.data);
+        setIsLoading(false);
+      });
     setCategogo("Men");
   };
 
@@ -75,19 +85,22 @@ let limit=8
       <SmallScreenSearchBar />
       <Navbar />
       <HomePage_Text_Carousel />
-      <LoadingComponent statusQuo={ isLoading } justifySelf={"center"}/>
+      <LoadingComponent statusQuo={isLoading} justifySelf={"center"} />
+      <Divider/>
       <Flex
         minWidth="max-content"
         alignItems="center"
         gap="2"
-        pl="40px"
-        pt="15px" mb="5px"
+        pl="30px"
+        py="5px"
+        mb="5px"
+        bgColor={"rgb(229,241,248)"}
       >
         <ButtonGroup gap="2">
-          <Button colorScheme="teal" onClick={handleClickWomen}>
+          <Button color="white" bgColor="rgb(0,118,189)" onClick={handleClickWomen}>
             Women
           </Button>
-          <Button colorScheme="teal" onClick={handleClickMen}>
+          <Button color="white" bgColor="rgb(0,118,189)" onClick={handleClickMen}>
             Men
           </Button>
         </ButtonGroup>
@@ -96,53 +109,63 @@ let limit=8
           <Heading size="md"></Heading>
         </Box>
       </Flex>
-      <Grid 
+      <Grid
         templateColumns={["repeat(2, 1fr)", "repeat(4, 1fr)"]}
         gap={2}
         p="25px"
-        boxShadow='base' rounded='md' bg='white' mb="5px"
+        boxShadow="base"
+        rounded="md"
+        bg="white"
+        mb="5px"
       >
         {products_data.map((item) => (
-          <GridItem
-            key={item.id}
-            w="auto"
-            h="auto"
-            mb={1}
-            // border="1px solid red"
-          >
-            <Box w="100%" h="auto">
-              <Image src={item.image} alt={item.title} />
-            </Box>
-            <Box w="100%" margin={"auto"} p="10px">
-              <Text
-                _hover={{
-                  color: "rgb(0,118,189)",
-                  textDecoration: "underline",
-                }}
-                fontWeight={500}
-              >
-                {item.title}
-              </Text>
-              <Text
-                _hover={{
-                  color: "rgb(115,173,230)",
-                  textDecoration: "underline",
-                  textDecoration: "underline",
-                }}
-                fontSize="16px"
-                fontWeight={100}
-              >
-                {item.description}
-              </Text>
-              <Text color={"rgb(3,93,89)"}>${item.price}</Text>
-            </Box>
-          </GridItem>
+          <Link to={`/products/${item.id}`}>
+            <GridItem
+              key={item.id}
+              w="auto"
+              h="auto"
+              mb={1}
+              // border="1px solid red"
+              // onClick={}
+            >
+              <Box w="100%" h="auto">
+                <Image src={item.image} alt={item.title} />
+              </Box>
+              <Box w="100%" margin={"auto"} p="10px">
+                <Text
+                  _hover={{
+                    color: "rgb(0,118,189)",
+                    textDecoration: "underline",
+                  }}
+                  fontWeight={500}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  _hover={{
+                    color: "rgb(115,173,230)",
+                    textDecoration: "underline",
+                    textDecoration: "underline",
+                  }}
+                  fontSize="16px"
+                  fontWeight={100}
+                >
+                  {item.description}
+                </Text>
+                <Text color={"rgb(3,93,89)"}>${item.price}</Text>
+              </Box>
+            </GridItem>
+          </Link>
         ))}
       </Grid>
       <Flex gap={1} pr="40px" mb="30px" justifyContent={"flex-end"}>
-        <Button disabled={page===1} onClick={()=>setPage(page-1)} >Prev</Button>
+        <Button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          Prev
+        </Button>
         <Button disabled={page}>{page}</Button>
-        <Button disabled={page===total} onClick={()=>setPage(page+1)} >Next</Button>
+        <Button disabled={page === total} onClick={() => setPage(page + 1)}>
+          Next
+        </Button>
         {/* <Pagination
           currentPage={page}
           totalPages={total}
--- zappos_com/src/Routes/Allroutes.jsx
+++ zappos_com/src/Routes/Allroutes.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home_Page from '../Components/Home_Page'
 import PrivateRoutes from '../Components/PrivateRoutes'
+import Error from '../Pages/Error'
 import Home from '../Pages/Home'
 import ProductPageOG from '../Pages/ProductPageOG'
 import Products_Page from '../Pages/Products_Page'
 import Sign_in_page from '../Pages/Sign_in_page'
+import SingleProductPage from '../Pages/SingleProductPage'
 
 const Allroutes = () => {
   return (
@@ -13,7 +15,8 @@ const Allroutes = () => {
         <Route path={"/"} element={<Home/>} ></Route>
         <Route path={"/signin"} element={<Sign_in_page/>}></Route>
         <Route path={"/products"} element={<PrivateRoutes><ProductPageOG/></PrivateRoutes>}></Route>
-    
+        <Route path={"/products/:id"} element={<PrivateRoutes><SingleProductPage/></PrivateRoutes>}></Route>
+    <Route path={"*"} element={<Error/>} ></Route>
     </Routes>
   )
 }
